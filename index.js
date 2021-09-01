aee4569d4478bb2e1107a63ddce317bfb459b06c
Lakisha J <lakishajohnson@pursuit.org>
2021-09-01 14:51:44 -0400

    Find and Filter Functions passed

--- index.js
+++ index.js
@@ -14,9 +14,18 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    
+    if (callback(element) === true) {
+      return(element)
+    }
   }
+  return undefined
 }
+  
+ 
+  
+    // Write your code here.
+  
 
 /**
  * Returns an array of all elements in the array that cause the callback to return `true`. If the array is empty or no elements cause the callback to return `true`, then return an empty array.
@@ -35,10 +44,16 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+ 
+    if (callback(element)) {
+      result.push(element)
+    }
   }
-  return result;
+  return result
 }
+ 
+      
+
 
 /**
  * Returns an array where each element is transformed by the callback. If the array is empty, return an empty array.
