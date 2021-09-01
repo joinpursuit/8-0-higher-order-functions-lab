546bcd2814649cb250a3c2ffe1a59b315bad7306
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-09-01 13:37:19 -0400

    adds find function

--- index.js
+++ index.js
@@ -14,8 +14,11 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element
+    }
   }
+  return undefined
 }
 
 /**
