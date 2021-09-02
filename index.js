fa2764911150418dfccfb09eb4bed27447324e8e
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-09-01 22:57:37 -0400

    all fn completed

--- index.js
+++ index.js
@@ -14,8 +14,11 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element;
+    }
   }
+  return undefined;
 }
 
 /**
@@ -36,6 +39,9 @@ function filter(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    if (callback(element)) {
+      result.push(element);
+    }
   }
   return result;
 }
@@ -58,6 +64,8 @@ function map(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    callback(element);
+    result.push(callback(element));
   }
   return result;
 }
@@ -78,6 +86,7 @@ function map(array, callback) {
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
     // Write your code here.
+    callback(array[i], i, array);
   }
 }
 
