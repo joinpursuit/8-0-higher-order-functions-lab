60e56af243826a396514b767bc9f0b935a88aef1
Bryant Mercedes <bryantmercedescanela@gmail.com>
2021-09-01 21:04:16 -0400

    Finished

--- index.js
+++ index.js
@@ -14,9 +14,12 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element;
+    }
   }
 }
+
 
 /**
  * Returns an array of all elements in the array that cause the callback to return `true`. If the array is empty or no elements cause the callback to return `true`, then return an empty array.
@@ -35,10 +38,13 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      result.push(element)
+    }
   }
   return result;
 }
+
 
 /**
  * Returns an array where each element is transformed by the callback. If the array is empty, return an empty array.
@@ -57,7 +63,7 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+  result.push(callback(element))
   }
   return result;
 }
@@ -77,7 +83,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i], i, array)
   }
 }
 
