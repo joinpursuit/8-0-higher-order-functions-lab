3f6bf4af459245fe64930955100889d6d2f895ae
Angie G <angieg@Angies-MBP.fios-router.home>
2021-09-07 22:30:19 -0400

    Higher Order Functions- find, filter, map, forEach

--- index.js
+++ index.js
@@ -13,10 +13,19 @@
  *  //> undefined
  */
 function find(array, callback) {
-  for (let element of array) {
-    // Write your code here.
+  for (let currentArr of array) {
+    if (callback(currentArr)) {
+    return currentArr;
+    }
   }
 }
+
+
+  // for (let element of array) {
+  //     if (element === callback) { 
+  //       return undefined
+  // }
+  // return callback(element);
 
 /**
  * Returns an array of all elements in the array that cause the callback to return `true`. If the array is empty or no elements cause the callback to return `true`, then return an empty array.
@@ -35,7 +44,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      result.push(element)
+    }
   }
   return result;
 }
@@ -57,7 +68,7 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    result.push(callback(element))
   }
   return result;
 }
@@ -77,7 +88,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+   callback(array[i], i, array)
   }
 }
 
