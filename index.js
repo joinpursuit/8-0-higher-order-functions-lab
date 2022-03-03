2afe739c465e8fe15685224b02c10727022c34ab
Stephanie Frias <steph.frias01@me.com>
2022-03-03 16:16:26 -0500

    completed lab

--- index.js
+++ index.js
@@ -13,10 +13,15 @@
  *  //> undefined
  */
 function find(array, callback) {
+  let result = undefined;
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element
+    }
+   }
+    return result;
   }
-}
+
 
 /**
  * Returns an array of all elements in the array that cause the callback to return `true`. If the array is empty or no elements cause the callback to return `true`, then return an empty array.
@@ -35,7 +40,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if(callback(element)){
+      result.push(element)
+    }
   }
   return result;
 }
@@ -57,7 +64,7 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    result.push(callback(element))
   }
   return result;
 }
@@ -77,7 +84,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i], i, array);
   }
 }
 
