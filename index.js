e0d7486138ea6d6f54993a9e36d71a0aaf8da0b1
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-03-06 15:22:12 -0500

    "Higher Functions"

--- index.js
+++ index.js
@@ -14,9 +14,15 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+  // console.log(callback(element));
+    if(callback(element) === true ){
+      return element;
+    } 
+      
+    }
+
   }
-}
+
 
 /**
  * Returns an array of all elements in the array that cause the callback to return `true`. If the array is empty or no elements cause the callback to return `true`, then return an empty array.
@@ -35,7 +41,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if(callback(element) === true){
+      result.push(element)
+    }
   }
   return result;
 }
@@ -57,7 +65,9 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    result.push(callback(element))
+
+
   }
   return result;
 }
@@ -77,7 +87,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i],i,array)
   }
 }
 
