1b250198eef83352d36df950ffa44758c8cc545b
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-03-06 14:50:44 -0500

    finished lab

--- index.js
+++ index.js
@@ -14,8 +14,11 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)){
+      return element;
+    }
   }
+  return undefined
 }
 
 /**
@@ -35,7 +38,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)){
+      result.push(element);
+    }
   }
   return result;
 }
@@ -57,7 +62,7 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    result.push(callback(element))
   }
   return result;
 }
@@ -76,8 +81,8 @@ function map(array, callback) {
  *  //> 30 2 3
  */
 function forEach(array, callback) {
-  for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+  for (let i = 0; i < array.length; i++){
+    callback(array[i], i, array);
   }
 }
 
