4550c30bc290ab4a4e239f9188c49c0252b9de63
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-03-07 00:26:57 -0400

    Finalized High Order Functions Lab.

--- index.js
+++ index.js
@@ -13,9 +13,14 @@
  *  //> undefined
  */
 function find(array, callback) {
+  
   for (let element of array) {
+    if(callback(element)) {
+      return element;
+    }
     // Write your code here.
   }
+  return undefined;
 }
 
 /**
@@ -36,6 +41,9 @@ function filter(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    if(callback(element)) {
+      result.push(element)
+    }
   }
   return result;
 }
@@ -57,6 +65,7 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
+    result.push(callback(element))
     // Write your code here.
   }
   return result;
@@ -78,7 +87,9 @@ function map(array, callback) {
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
     // Write your code here.
+    callback(array[i], i, array)
   }
+
 }
 
 // Do not change the code below this line.
