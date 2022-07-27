e0326bb790ac23a3f8546b94213b2dc155299d39
Gillesedgy <edgygilles@pursuit.org>
2022-07-27 14:53:45 -0400

    complete

--- index.js
+++ index.js
@@ -88,7 +88,9 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    // callback.push($array[i])
+
+    console.log(callback(array[i],i, array))
   }
 }
 
