475de7d95c83ec2928b99802b1c30cfd21bba281
Fadila-Ali <fadilaali@pursuit.org>
2022-07-27 15:32:31 -0400

    high order functions lab

--- index.js
+++ index.js
@@ -14,8 +14,9 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) return element;
   }
+  return undefined;
 }
 
 /**
@@ -35,7 +36,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      result.push(element);
+    }
   }
   return result;
 }
@@ -56,10 +59,9 @@ function filter(array, callback) {
  */
 function map(array, callback) {
   const result = [];
-  for (let element of array) {
-    // Write your code here.
-  }
-  return result;
+  for (let element of array) 
+    result.push(callback(element));
+    return result;
 }
 
 /**
@@ -77,7 +79,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i], i, array);
   }
 }
 
