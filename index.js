1fd8ecb654770c92c1f00476ebb64e9ea2bcf85b
Karyn Huston <huston.karyn@gmail.com>
2022-07-28 00:17:13 -0400

    last changes for lab

--- index.js
+++ index.js
@@ -15,7 +15,9 @@
 function find(array, callback) {
   for (let element of array) {
     // Write your code here.
+    if (callback(element)) return element;
   }
+  return undefined;
 }
 
 /**
@@ -36,6 +38,7 @@ function filter(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    if (callback(element)) result.push(element);
   }
   return result;
 }
@@ -58,6 +61,7 @@ function map(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    result.push(callback(element));
   }
   return result;
 }
@@ -78,6 +82,7 @@ function map(array, callback) {
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
     // Write your code here.
+    callback(array[i], i, callback);
   }
 }
 
