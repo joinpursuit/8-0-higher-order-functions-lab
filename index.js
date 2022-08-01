2ec8eb7dbc010b63bd4a36a1521b106d71dabb6a
Rebecca Garcia <shelteredheart21@gmail.com>
2022-08-01 11:04:10 -0400

    done

--- index.js
+++ index.js
@@ -13,9 +13,13 @@
  *  //> undefined
  */
 function find(array, callback) {
+
   for (let element of array) {
     // Write your code here.
+
+    if (callback(element)) return element
   }
+  return undefined;
 }
 
 /**
@@ -36,6 +40,9 @@ function filter(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    if (callback(element)){
+
+    result.push(element);
   }
   return result;
 }
@@ -58,6 +65,7 @@ function map(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    result.push(callback(element));
   }
   return result;
 }
@@ -78,7 +86,9 @@ function map(array, callback) {
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
     // Write your code here.
+    callback(array[i], i, array); 
   }
+}
 }
 
 // Do not change the code below this line.
