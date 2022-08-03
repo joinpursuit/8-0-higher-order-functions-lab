39af913cf076b66228b1f114ab6cf7091e7e170c
bigdaddyjean <jeannappoleon@pursuit.org>
2022-08-02 21:12:56 -0400

    finished

--- index.js
+++ index.js
@@ -12,10 +12,12 @@
  *  find([1, 2, 3], (element) => element < 0);
  *  //> undefined
  */
-function find(array, callback) {
+ function find(array, callback) {
   for (let element of array) {
     // Write your code here.
+    if (callback(element)) return element;
   }
+  return undefined;
 }
 
 /**
@@ -36,6 +38,7 @@ function filter(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    if (callback(element)) result.push(element);
   }
   return result;
 }
@@ -58,6 +61,8 @@ function map(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
+    const updated = callback(element);
+    result.push(updated);
   }
   return result;
 }
@@ -78,6 +83,7 @@ function map(array, callback) {
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
     // Write your code here.
+    callback(array[i], i, array)
   }
 }
 
