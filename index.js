6554f64c5a97cbd5bc1a9e13d9103985a0cce232
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2022-11-16 13:15:39 -0500

    Passes first two tests

--- index.js
+++ index.js
@@ -14,7 +14,9 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if(callback(element)){
+      return element
+    }
   }
 }
 
@@ -35,7 +37,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if(callback(element)){
+      result.push(element)
+    }
   }
   return result;
 }
