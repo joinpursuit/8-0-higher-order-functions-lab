5e97c5dab594002b0f5f2376474d5f0019ea0d8a
Isaiah Desrosiers <isaiahdesrosiers21@gmail.com>
2022-11-16 17:58:19 -0500

    filter function.

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)){
+      result.push(element);
+    }
   }
   return result;
 }
