5ec17576ce6164f822298320875d0855b8db1198
YasserS-21 <saadi.yasser04@gmail.com>
2022-11-17 00:31:44 -0500

    code upload

--- index.js
+++ index.js
@@ -14,6 +14,9 @@
  */
 function find(array, callback) {
   for (let element of array) {
+    if (callback(element)) {
+      return element
+    }
     // Write your code here.
   }
 }
@@ -35,6 +38,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
+    if (callback(element)){
+      result.push(element)
+    }
     // Write your code here.
   }
   return result;
@@ -57,6 +63,7 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
+      result.push(callback(element))
     // Write your code here.
   }
   return result;
@@ -76,8 +83,9 @@ function map(array, callback) {
  *  //> 30 2 3
  */
 function forEach(array, callback) {
+
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i],i,array)
   }
 }
 
