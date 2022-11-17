e9fcbb3350357f489f24eab2d1aadf37fe3de4b0
jkpasaoa <jacqueline.pasaoa@gmail.com>
2022-11-16 19:54:44 -0500

    second function. function filter. done

--- index.js
+++ index.js
@@ -37,7 +37,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+   if (callback(element)) {
+    result.push(element)
+   }
   }
   return result;
 }
